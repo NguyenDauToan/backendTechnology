aece7c2087c386902cc98abdc2bef2c32adf05e3
Toan Nguyen <dtoan.aeck@gmail.com>
2026-04-25 21:47:51 +0700

    fix

--- src/models/User.js
+++ src/models/User.js
@@ -31,8 +31,8 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Middleware hash password trước khi lưu
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password') || !this.password) return next();
+userSchema.pre('save', async function () {
+  if (!this.isModified('password') || !this.password) return;
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
